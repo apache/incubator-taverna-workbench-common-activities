d65d34d0bb9dcef998c100423f5adc47abfa1958
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-17 14:04:08 +0000

    confirming to async interface status messages while it's updating
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/trunk/wsdl-activity-ui@7288 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/wsdl/servicedescriptions/WSDLServiceProvider.java
+++ src/main/java/net/sf/taverna/t2/activities/wsdl/servicedescriptions/WSDLServiceProvider.java
@@ -3,6 +3,8 @@ package net.sf.taverna.t2.activities.wsdl.servicedescriptions;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JOptionPane;
@@ -11,7 +13,7 @@ import javax.wsdl.WSDLException;
 import javax.xml.parsers.ParserConfigurationException;
 
 import net.sf.taverna.t2.activities.wsdl.WSDLActivityConfigurationBean;
-import net.sf.taverna.t2.servicedescriptions.AbstractConfigurableServiceDescriptionProvider;
+import net.sf.taverna.t2.servicedescriptions.AbstractConfigurableServiceProvider;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescription;
 import net.sf.taverna.wsdl.parser.UnknownOperationException;
 import net.sf.taverna.wsdl.parser.WSDLParser;
@@ -19,53 +21,68 @@ import net.sf.taverna.wsdl.parser.WSDLParser;
 import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 
-public class WSDLServiceProvider
-		extends
-		AbstractConfigurableServiceDescriptionProvider<WSDLServiceProviderConfig> {
+public class WSDLServiceProvider extends
+		AbstractConfigurableServiceProvider<WSDLServiceProviderConfig> {
 
+	private static final String WSDL_SERVICE = "WSDL service";
 	private static Logger logger = Logger.getLogger(WSDLServiceProvider.class);
 
-	public List<ServiceDescription<WSDLActivityConfigurationBean>> getServiceDescriptions() {
+	public WSDLServiceProvider() {
+		super(new WSDLServiceProviderConfig("http://somehost/service?wsdl"));
+	}
+
+	public String getName() {
+		return WSDL_SERVICE;
+	}
+
+	public List<WSDLServiceProviderConfig> getDefaultConfigurations() {
+		List<WSDLServiceProviderConfig> defaults = new ArrayList<WSDLServiceProviderConfig>();
+		// TODO: Defaults should come from a config/resource file
+		defaults.add(new WSDLServiceProviderConfig(
+				"http://soap.genome.jp/KEGG.wsdl"));
+		defaults.add(new WSDLServiceProviderConfig(
+				"http://soap.bind.ca/wsdl/bind.wsdl"));
+		return defaults;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void findServiceDescriptionsAsync(
+			FindServiceDescriptionsCallBack callBack) {
 		List<ServiceDescription<WSDLActivityConfigurationBean>> descriptions = new ArrayList<ServiceDescription<WSDLActivityConfigurationBean>>();
 		URI wsdl = serviceProviderConfig.getURI();
-		logger.info("About to parse wsdl:" + wsdl);
+		callBack.status("About to parse wsdl:" + wsdl);
 		WSDLParser parser = null;
 		try {
 			parser = new WSDLParser(wsdl.toASCIIString());
 			List<Operation> operations = parser.getOperations();
+			callBack.status("Finding WSDL operations:" + wsdl);
+			List<ServiceDescription> items = new ArrayList<ServiceDescription>();
 			for (Operation op : operations) {
 				WSDLServiceDescription item = new WSDLServiceDescription();
 				try {
 					item.setOperation(op.getName());
 					item.setUse(parser.getUse(item.getOperation()));
 					item.setStyle(parser.getStyle());
-					item.setURI(wsdl);					
-					descriptions.add(item);
+					item.setURI(wsdl);
+					items.add(item);
 				} catch (UnknownOperationException e) {
 					String message = "Encountered an unexpected operation name:"
 							+ item.getOperation();
-					logger.error(message, e);
-					JOptionPane.showMessageDialog(null, message, "ERROR",
-							JOptionPane.ERROR_MESSAGE);
+					callBack.fail(message, e);
 				}
 			}
+			callBack.partialResults(items);
+			callBack.finished();
 		} catch (ParserConfigurationException e) {
 			String message = "Error configuring the WSDL parser";
-			logger.error(message, e);
-			JOptionPane.showMessageDialog(null, message, "ERROR",
-					JOptionPane.ERROR_MESSAGE);
+			callBack.fail(message, e);
 		} catch (WSDLException e) {
 			String message = "There was an error with the wsdl: " + wsdl;
-			logger.error(message, e);
-			JOptionPane.showMessageDialog(null, message, "ERROR",
-					JOptionPane.ERROR_MESSAGE);
+			callBack.fail(message, e);
 		} catch (IOException e) {
-			String[] message = new String[] {
-					"There was an IO error parsing the wsdl: " + wsdl,
-					"Possible reason: the wsdl location was incorrect." };
-			logger.error(message, e);
-			JOptionPane.showMessageDialog(null, message, "ERROR",
-					JOptionPane.ERROR_MESSAGE);
+			String message = "There was an IO error parsing the wsdl: " + wsdl
+					+ " Possible reason: the wsdl location was incorrect.";
+			callBack.fail(message, e);
 		} catch (SAXException e) {
 			String message = "There was an error with the XML in the wsdl: "
 					+ wsdl;
@@ -73,29 +90,18 @@ public class WSDLServiceProvider
 			JOptionPane.showMessageDialog(null, message, "ERROR",
 					JOptionPane.ERROR_MESSAGE);
 		} catch (IllegalArgumentException e) { // a problem with the wsdl url
-			String[] message = new String[] {
-					"There was an error with the wsdl: " + wsdl,
-					"Possible reason: the wsdl location was incorrect." };
-			logger.error(message, e);
-			JOptionPane.showMessageDialog(null, message, "ERROR",
-					JOptionPane.ERROR_MESSAGE);
+			String message = "There was an error with the wsdl: " + wsdl + " "
+					+ "Possible reason: the wsdl location was incorrect.";
+			callBack.fail(message, e);
 		} catch (Exception e) { // anything else we did not expect
 			String message = "There was an error with the wsdl: " + wsdl;
-			logger.error(message, e);
-			JOptionPane.showMessageDialog(null, message, "ERROR",
-					JOptionPane.ERROR_MESSAGE);
+			callBack.fail(message, e);
 		}
-		logger.info("Finished parsing WSDL:" + wsdl);
-		return descriptions;
 	}
 
-	public List<WSDLServiceProviderConfig> getDefaultConfigurations() {
-		List<WSDLServiceProviderConfig> defaults = new ArrayList<WSDLServiceProviderConfig>();
-		defaults.add(new WSDLServiceProviderConfig(
-				"http://soap.genome.jp/KEGG.wsdl"));
-		defaults.add(new WSDLServiceProviderConfig(
-				"http://soap.bind.ca/wsdl/bind.wsdl"));
-		return defaults;
+	@Override
+	public String toString() {
+		return getName() + " " + getConfiguration().getURI();
 	}
 
 }
