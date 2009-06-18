e9dad0ffbe05051645694b95fa864d5e858e4b65
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-06-18 10:30:28 +0000

    T2-656 Flush WSDL cache on removal of WSDL provider from service panel Service registry sets 'serviceProviderRegistry' property on the provider - if needed.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/trunk/wsdl-activity-ui@7824 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/wsdl/servicedescriptions/WSDLServiceProvider.java
+++ src/main/java/net/sf/taverna/t2/activities/wsdl/servicedescriptions/WSDLServiceProvider.java
@@ -7,12 +7,16 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.wsdl.Operation;
 import javax.wsdl.WSDLException;
 import javax.xml.parsers.ParserConfigurationException;
 
+import net.sf.taverna.t2.lang.observer.Observable;
+import net.sf.taverna.t2.lang.observer.Observer;
 import net.sf.taverna.t2.servicedescriptions.AbstractConfigurableServiceProvider;
+import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionRegistry;
+import net.sf.taverna.t2.servicedescriptions.events.RemovedProviderEvent;
+import net.sf.taverna.t2.servicedescriptions.events.ServiceDescriptionRegistryEvent;
 import net.sf.taverna.wsdl.parser.UnknownOperationException;
 import net.sf.taverna.wsdl.parser.WSDLParser;
 
@@ -22,11 +26,32 @@ import org.xml.sax.SAXException;
 public class WSDLServiceProvider extends
 		AbstractConfigurableServiceProvider<WSDLServiceProviderConfig> {
 
-	private static final String WSDL_SERVICE = "WSDL service";
-	
-	@SuppressWarnings("unused")
 	private static Logger logger = Logger.getLogger(WSDLServiceProvider.class);
 
+	public static class FlushWSDLCacheOnRemovalObserver implements
+			Observer<ServiceDescriptionRegistryEvent> {
+		public void notify(
+				Observable<ServiceDescriptionRegistryEvent> registry,
+				ServiceDescriptionRegistryEvent event) throws Exception {
+			if (!(event instanceof RemovedProviderEvent)) {
+				return;
+			}
+			RemovedProviderEvent removedProviderEvent = (RemovedProviderEvent) event;
+			if (!(removedProviderEvent.getProvider() instanceof WSDLServiceProvider)) {
+				return;
+			}
+			WSDLServiceProvider serviceProvider = (WSDLServiceProvider) removedProviderEvent
+					.getProvider();
+			URI wsdlLocation = serviceProvider.getConfiguration().getURI();
+			WSDLParser.flushCache(wsdlLocation.toASCIIString());
+			logger.info("Flushed cache for WSDL " + wsdlLocation);
+		}
+	}
+
+	private static final String WSDL_SERVICE = "WSDL service";
+
+	private static FlushWSDLCacheOnRemovalObserver flushObserver = new FlushWSDLCacheOnRemovalObserver();
+	
 	public WSDLServiceProvider() {
 		super(new WSDLServiceProviderConfig("http://somehost/service?wsdl"));
 	}
@@ -39,11 +64,12 @@ public class WSDLServiceProvider extends
 		List<WSDLServiceProviderConfig> defaults = new ArrayList<WSDLServiceProviderConfig>();
 		// TODO: Defaults should come from a config/resource file
 		defaults.add(new WSDLServiceProviderConfig(
-		"http://www.ebi.ac.uk/xembl/XEMBL.wsdl"));
+				"http://www.ebi.ac.uk/xembl/XEMBL.wsdl"));
 		defaults.add(new WSDLServiceProviderConfig(
-		"http://soap.genome.jp/KEGG.wsdl"));
-		defaults.add(new WSDLServiceProviderConfig(
-				"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/soap/eutils.wsdl"));
+				"http://soap.genome.jp/KEGG.wsdl"));
+		defaults
+				.add(new WSDLServiceProviderConfig(
+						"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/soap/eutils.wsdl"));
 		defaults.add(new WSDLServiceProviderConfig(
 				"http://soap.bind.ca/wsdl/bind.wsdl"));
 		defaults.add(new WSDLServiceProviderConfig(
@@ -59,16 +85,18 @@ public class WSDLServiceProvider extends
 		try {
 			parser = new WSDLParser(wsdl.toASCIIString());
 			List<Operation> operations = parser.getOperations();
-			callBack.status("Found " + operations.size() + " WSDL operations:" + wsdl);
+			callBack.status("Found " + operations.size() + " WSDL operations:"
+					+ wsdl);
 			List<WSDLServiceDescription> items = new ArrayList<WSDLServiceDescription>();
 			for (Operation op : operations) {
 				WSDLServiceDescription item = new WSDLServiceDescription();
 				try {
 					item.setOperation(op.getName());
 					item.setUse(parser.getUse(op.getName()));
-					item.setStyle(parser.getStyle());					
+					item.setStyle(parser.getStyle());
 					item.setURI(wsdl);
-					item.setDescription(parser.getOperationDocumentation(op.getName()));
+					item.setDescription(parser.getOperationDocumentation(op
+							.getName()));
 					items.add(item);
 				} catch (UnknownOperationException e) {
 					String message = "Encountered an unexpected operation name:"
@@ -116,6 +144,21 @@ public class WSDLServiceProvider extends
 		List<String> result;
 		result = Arrays.asList(getConfiguration().getURI().toString());
 		return result;
+	}
+	
+	/**
+	 * Will be set by ServiceDescriptionRegistryImpl 
+	 * 
+	 * @param registry Registry this provider has been added to
+	 */
+	public void setServiceDescriptionRegistry(
+			ServiceDescriptionRegistry registry) {
+		synchronized (flushObserver) {
+			// Add the (static common) observer if the registry does not have it			
+			if (!registry.getObservers().contains(flushObserver)) {
+				registry.addObserver(flushObserver);
+			}
+		}
 	}
 
 }
