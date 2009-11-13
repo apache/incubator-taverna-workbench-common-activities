758f4fc6a01ee01c4fd74ffef1c62df7439704d8
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-11-13 17:53:13 +0000

    removed unused string
    git-svn-id: http://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/trunk/beanshell-activity-ui@9171 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/beanshell/actions/BeanshellActivityConfigurationAction.java
+++ src/main/java/net/sf/taverna/t2/activities/beanshell/actions/BeanshellActivityConfigurationAction.java
@@ -32,7 +32,6 @@ import net.sf.taverna.t2.workbench.ui.views.contextualviews.activity.ActivityCon
 @SuppressWarnings("serial")
 public class BeanshellActivityConfigurationAction extends ActivityConfigurationAction<BeanshellActivity, BeanshellActivityConfigurationBean>{
 
-	private static final String BEANSHELL_ACTIVITY_CONFIGURATION = "Beanshell Service Configuration";
 	public static final String EDIT_BEANSHELL_SCRIPT = "Edit beanshell script";
 
 	public BeanshellActivityConfigurationAction(BeanshellActivity activity, Frame owner) {
