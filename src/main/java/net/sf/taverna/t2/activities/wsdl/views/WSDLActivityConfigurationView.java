591467329a47b5d50aa68fd4ef52d1d2ec8e3efa
alex@mygrid.org.uk <alex@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-01-28 16:19:20 +0000

    Removed some unused imports.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/branches/maintenance/wsdl-activity-ui@12351 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/wsdl/views/WSDLActivityConfigurationView.java
+++ src/main/java/net/sf/taverna/t2/activities/wsdl/views/WSDLActivityConfigurationView.java
@@ -41,7 +41,6 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
