6f8dde148977dce8530adac11374d3442b5b23ef
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-04-20 11:21:51 +0000

    Added menu items for adding external tool
    git-svn-id: http://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/branches/maintenance/external-tool-activity-ui@12932 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/java/net/sf/taverna/t2/activities/externaltool/menu/AddExternalToolContextualMenuAction.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (C) 2007-2009 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.externaltool.menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.net.URI;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+
+import net.sf.taverna.t2.activities.externaltool.ExternalToolActivity;
+import net.sf.taverna.t2.activities.externaltool.servicedescriptions.ExternalToolTemplateServiceDescription;
+import net.sf.taverna.t2.ui.menu.AbstractContextualMenuAction;
+import net.sf.taverna.t2.workbench.activityicons.ActivityIconManager;
+import net.sf.taverna.t2.workbench.file.FileManager;
+import net.sf.taverna.t2.workbench.ui.workflowview.WorkflowView;
+import net.sf.taverna.t2.workflowmodel.Dataflow;
+
+import org.apache.log4j.Logger;
+
+/**
+ * An action to add an external tool + a wrapping processor to the workflow.
+ * 
+ * @author Alex Nenadic
+ * @author Alan Williamns
+ * 
+ */
+@SuppressWarnings("serial")
+public class AddExternalToolContextualMenuAction extends AbstractContextualMenuAction {
+
+	private static final String ADD_EXTERNAL_TOOL = "External tool";
+
+	private static final URI insertSection = URI
+			.create("http://taverna.sf.net/2009/contextMenu/insert");
+
+	private static Logger logger = Logger
+			.getLogger(AddExternalToolMenuAction.class);
+
+	public AddExternalToolContextualMenuAction() {
+		super(insertSection, 55);
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return super.isEnabled()
+				&& getContextualSelection().getSelection() instanceof Dataflow;
+	}
+
+	@Override
+	protected Action createAction() {
+
+		return new AddExternalToolAction();
+	}
+	
+	protected class AddExternalToolAction extends AbstractAction {
+		AddExternalToolAction () {
+			super (ADD_EXTERNAL_TOOL, ActivityIconManager.getInstance().iconForActivity(
+						new ExternalToolActivity()));
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			Dataflow workflow = FileManager.getInstance()
+			.getCurrentDataflow();
+
+	WorkflowView.importServiceDescription(ExternalToolTemplateServiceDescription.getServiceDescription(),
+			false);
+		}
+	}
+
+}
---
+++ src/main/java/net/sf/taverna/t2/activities/externaltool/menu/AddExternalToolMenuAction.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (C) 2007-2009 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.externaltool.menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.net.URI;
+
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+import net.sf.taverna.t2.activities.externaltool.ExternalToolActivity;
+import net.sf.taverna.t2.activities.externaltool.servicedescriptions.ExternalToolTemplateServiceDescription;
+import net.sf.taverna.t2.ui.menu.AbstractMenuAction;
+import net.sf.taverna.t2.workbench.activityicons.ActivityIconManager;
+import net.sf.taverna.t2.workbench.ui.workflowview.WorkflowView;
+import net.sf.taverna.t2.workbench.views.graph.actions.DesignOnlyAction;
+import net.sf.taverna.t2.workbench.views.graph.menu.InsertMenu;
+
+import org.apache.log4j.Logger;
+
+/**
+ * An action to add a externaltool activity + a wrapping processor to the workflow.
+ * 
+ * @author Alex Nenadic
+ * @author alanrw
+ * 
+ */
+@SuppressWarnings("serial")
+public class AddExternalToolMenuAction extends AbstractMenuAction {
+
+	private static final String ADD_EXTERNAL_TOOL = "External tool";
+
+	private static final URI ADD_EXTERNAL_TOOL_URI = URI
+	.create("http://taverna.sf.net/2008/t2workbench/menu#graphMenuAddExternalTool");
+
+	private static Logger logger = Logger
+			.getLogger(AddExternalToolMenuAction.class);
+
+	public AddExternalToolMenuAction() {
+		super(InsertMenu.INSERT, 21, ADD_EXTERNAL_TOOL_URI);
+	}
+
+	@Override
+	protected Action createAction() {
+
+		return new AddExternalToolAction();
+	}
+	
+	protected class AddExternalToolAction extends DesignOnlyAction {
+		AddExternalToolAction () {
+			super ();
+			putValue(SMALL_ICON, ActivityIconManager.getInstance().iconForActivity(
+					new ExternalToolActivity()));
+			putValue(NAME, ADD_EXTERNAL_TOOL);	
+			putValue(SHORT_DESCRIPTION, "External tool");	
+			putValue(Action.ACCELERATOR_KEY,
+					KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.SHIFT_DOWN_MASK | InputEvent.ALT_DOWN_MASK));
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			WorkflowView.importServiceDescription(ExternalToolTemplateServiceDescription.getServiceDescription(),
+			false);
+		}
+	}
+
+}
