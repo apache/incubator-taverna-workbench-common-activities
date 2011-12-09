b7e75db05930b769bba027c5146897632396bd9f
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-12-09 15:18:51 +0000

    git-svn-id: https://taverna.googlecode.com/svn/taverna/ui/net.sf.taverna.t2.ui-activities/branches/maintenance/wsdl-activity-ui@14323 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/wsdl/views/AbstractXMLSplitterActionView.java
+++ src/main/java/net/sf/taverna/t2/activities/wsdl/views/AbstractXMLSplitterActionView.java
@@ -151,8 +151,6 @@ public abstract class AbstractXMLSplitterActionView<BeanType> extends
             html+="<code>"+descriptor.getQname().toString()+"</code><br>";
             if (descriptor.getDocumentation() != null && !descriptor.getDocumentation().isEmpty()){
                 html += "<p>"+descriptor.getDocumentation()+"</p>";
-            } else {
-            	System.err.println("No description for " + port.getName() + "\n");
             }
         }
 
